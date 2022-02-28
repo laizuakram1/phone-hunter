b1cc300e00e101535287c87a18c5515aeb1bc225
laizuakram1 <laizuakram98@gmail.com>
2022-02-28 21:49:16 +0600

    add phone image, title and brand name

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ const loadPhones = (phone) => {
     .then(data => displayPhones(data.data.slice(0, 20)))
 }
 
+const displayPhones = (data) => {
+    for(const phone of data){
+        console.log(phone.brand);
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${phone.phone_name}</h5>
+                  <p class="card-text">${phone.brand}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        `
+        phoneContainer.appendChild(div);
+    }
+}
