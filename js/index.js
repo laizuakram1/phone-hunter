f3d0dc57301d1b14829a90f491c6d28e0ec93d65
laizuakram1 <laizuakram98@gmail.com>
2022-03-07 22:04:00 +0600

    add user message

--- js/index.js
+++ js/index.js
@@ -1,32 +1,66 @@
 const phoneContainer = document.getElementById('display-phones');
+const singlePhoneContainer = document.getElementById('single-phone');
+const userMessage = document.getElementById('message');
 
 const searchButton = () => {
     const inputField = document.getElementById('input-field');
     const inputText = loadPhones(inputField.value);
-
+    if(inputField.value = ' '){
+        userMessage.innerText = `*please type a phone name`;
+        
+        
+     }
+   
+    
+    // clear data
+    inputField.value = ' ';
 }
+
+
 
 const loadPhones = (phone) => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${phone}`)
     .then(res => res.json())
-    .then(data => displayPhones(data.data.slice(0, 20)))
+    .then(data => displayPhones(data.data))
 }
+
+const clickButton = (para) => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${para}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+// const loadsinglePhone = (para) =>{
+//     console.log(para.data.slug);
+//     fetch(`https://openapi.programming-hero.com/api/phone/${para.data.slug}`)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// }
 
 const displayPhones = (data) => {
     for(const phone of data){
-        console.log(phone.brand);
+        // console.log(phone.phone_name);
         const div = document.createElement('div');
         div.classList.add('col')
+      
         div.innerHTML = `
             <div class="card my-4 border-0" style="width: 18rem;">
                 <img height="450px" src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">${phone.phone_name}</h5>
                   <p class="card-text">${phone.brand}</p>
-                  <button>More Details</button>
+                  <button onclick="clickButton(${phone.phone_name})" class="btn btn-primary">More Details</button>
                 </div>
               </div>
         `
         phoneContainer.appendChild(div);
+
     }
+    
 }
+// // click button 
+// const clickButton = id => {
+//     console.log(id);
+//     fetch(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// }
